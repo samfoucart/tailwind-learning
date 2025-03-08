13e02a1df7e0f5991e83a219a51c36afd75efadf
Sam Foucart <samfoucart25@gmail.com>
2025-03-07 23:05:12 -0500

    Added blog-like page

--- src/Pages/BlogPosts/BlogPosts.tsx
+++ src/Pages/BlogPosts/BlogPosts.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 export function BlogPosts() {
+  const [blogPost, setBlogPost] = useState<string | null>(null);
+  async function fetchPost() {
+    const response = await fetch(
+      `blog-posts/crafting-interpreters-part-1-review.md`
+    );
+
+    if (response.ok) {
+      const responseText = await response.text();
+      setBlogPost(responseText);
+    }
+  }
+
   return (
     <>
       <p>Hello</p>
+      <button onClick={fetchPost}>hello</button>
+      {blogPost == null ? <p>no post</p> : <p>{blogPost}</p>}
     </>
   );
 }
